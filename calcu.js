1a454783cdfa39018790939157955b9479e3851a
Nitish788 <nsinghrajput78@gmail.com>
2021-08-09 22:28:38 +0530

    initial commit

---
+++ calcu.js
@@ -0,0 +1,10 @@
+"use strict";
+const btnDigit = document.querySelectorAll(".btn-digit");
+let see = (document.querySelector(".screen").value =
+    document.querySelectorAll(".btn-digit"));
+
+// for (let i = 0; i < btnDigit.length; i++)
+//     btnDigit[i].addEventListener("click", function() {
+//         see = btnDigit[i];
+//     });
+console.log(see);
